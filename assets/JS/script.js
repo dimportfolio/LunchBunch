b937992644edb80584a51db5793134500556014e
Mannan Qazi <mannanqazi2@gmail.com>
2019-04-01 23:02:32 -0400

    added second spoonacular api and changed loops

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -28,30 +28,34 @@ $(document).ready(function() {
     }).then(function(response) {
       console.log(response);
 
-      for (var i = 0; i < response.results.length; i++) {
+      var i = 0;
+      while (i < response.results.length) {
         // get the title of the recipe (get rid of the extra text)
         var titleArray = response.results[i].title.split(' ');
 
         var title = titleArray[0] + ' ' + titleArray[1];
 
+        // put entire card structure inside one string and then append that to the recipe div. Put the id of an individual recipe inside the search button i.e. <a> tags under id attribute, for later access
         var html = `<div class="card" style="width: 20em; height: 25em; float: left; margin-left: 1em; margin-bottom: 1em">
-                       <img class="card-img-top" src=${response.baseUri +
-                         response.results[i]
-                           .image} style="width: 100%; height:12.5rem">
-                        <div class="card-body">
-                            <p class="card-text">${title}</p>
-                            <a class="btn btn-primary text-light btn-block recipeInd" id=${
-                              response.results[i].id
-                            }>Check Out Full Recipe!</a>
-                        </div>
-                    </div>`;
+                        <img class="card-img-top" src=${response.baseUri +
+                          response.results[i]
+                            .image} style="width: 100%; height:12.5rem">
+                         <div class="card-body">
+                             <p class="card-text">${title}</p>
+                             <a class="btn btn-primary text-light btn-block recipeInd" id=${
+                               response.results[i].id
+                             }>Check Out Full Recipe!</a>
+                         </div>
+                     </div>`;
 
         $('#main-recipe-body').append(html);
-
-        $('#ingredient').val('');
-        $('#filter').val('');
-        $('#exclude-ingredient').val('');
+        i++;
       }
+      // empty out the form fields
+
+      $('#ingredient').val('');
+      $('#filter').val('');
+      $('#exclude-ingredient').val('');
     });
   });
 
@@ -74,9 +78,22 @@ $(document).ready(function() {
         'X-RapidAPI-Key': '36b1955be7msh1566b339f906515p10b495jsn27ecc72ce407'
       }
     }).then(function(res) {
+      // in response, get the list of ingredients and store it in an object?
       console.log(res);
+      var ingredients = [];
+      var ingredientArray = res.extendedIngredients;
+      var k = 0;
+      while (k < ingredientArray.length) {
+        ingredients.push(ingredientArray[k].name);
+        k++;
+      }
+
+      console.log(ingredients);
+      //return ingredients;
     });
   }
 
   $(document).on('click', '.recipeInd', displayRecipe);
+
+  // Take care of nutritionix API now
 });
